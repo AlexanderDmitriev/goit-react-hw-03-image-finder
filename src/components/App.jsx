f6fa7d7a01fdf2232594065d0d75a4d8e6171ef3
AlexanderDmitriev <alex9110077@gmail.com>
2022-04-16 23:13:39 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,24 +5,44 @@ import * as API from './servicies/api';
 import {Container} from './App.styled';
 import GlobalStyle from '../GlobalStyle';
 
+//let totalHits=0;
+
 export class App extends Component{
 state = {
   keyWord:'',
+  id:'',
+  webformatURL:'',
+  largeImageURL:'',
+  totalHits:0,
+  data:null,
 };
 
 handleSearch = (values) => {
-  console.log(values);
-  API.getImages(values).then(function (response) {
+  API.getImages(values).then(response => {
     console.log(response.data.hits);
+    this.setState({totalHits:response.data.totalHits})
+    console.log(this.state.totalHits);
+    /* this.setState(prevState => ({ 
+      id:response.data.hits.id,
+      webformatURL:response.data.hits.webformatURL,
+      largeImageURL:response.data.hits.largeImageURL,
+    })); */
+    console.log(this.state);
   })
 };
+
+dataMaker = (values) => {
+
+};
+
 render(){
   return (
     <>
       <GlobalStyle/>
       <Container>
         <Searchbar onSubmit={this.handleSearch}/>
-        <ImageGallery/>
+        {(this.state.totalHits>0)&&<ImageGallery/>}
+        
       </Container>
     </>
   );
