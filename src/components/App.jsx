9a21a0933c81cc31cf802b5bf660c6b89f0a1d80
AlexanderDmitriev <alex9110077@gmail.com>
2022-04-17 18:20:19 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,25 +14,41 @@ state = {
   webformatURL:'',
   largeImageURL:'',
   totalHits:0,
-  data:null,
+  dataFromServer:null,
+  loading:false,
+};
+
+ componentDidMount(){
+  /* API.getImages("cat").then(response => {
+    console.log(response.data.hits);
+    this.setState({totalHits:response.data.totalHits})
+    console.log("this.state.totalHits: ",this.state.totalHits);
+     this.setState(prevState => ({ 
+      id:response.data.hits.id,
+      webformatURL:response.data.hits.webformatURL,
+      largeImageURL:response.data.hits.largeImageURL,
+    })); 
+    console.log(this.state);
+  }); */
+  this.setState({loading:true});
+  fetch('https://pixabay.com/api/?key=25236091-8685fe5809d54541c15ad7685&q=cat&page=1&image_type=photo&orientation=horizontal&per_page=12')
+  .then(res=>res.json())
+  .then(dataFromServer=>this.setState({dataFromServer}))
+  .finally(()=>this.setState({loading:false}));
 };
 
 handleSearch = (values) => {
   API.getImages(values).then(response => {
     console.log(response.data.hits);
     this.setState({totalHits:response.data.totalHits})
-    console.log(this.state.totalHits);
+    console.log("this.state.totalHits: ",this.state.totalHits);
     /* this.setState(prevState => ({ 
       id:response.data.hits.id,
       webformatURL:response.data.hits.webformatURL,
       largeImageURL:response.data.hits.largeImageURL,
     })); */
     console.log(this.state);
-  })
-};
-
-dataMaker = (values) => {
-
+  }) 
 };
 
 render(){
@@ -41,7 +57,8 @@ render(){
       <GlobalStyle/>
       <Container>
         <Searchbar onSubmit={this.handleSearch}/>
-        {(this.state.totalHits>0)&&<ImageGallery/>}
+        {(this.state.dataFromServer)&&<ImageGallery/>}
+        {(this.state.loading)&&<h1>Loading</h1>}
         
       </Container>
     </>
